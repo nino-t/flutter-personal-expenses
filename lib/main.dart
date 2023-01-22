d9a94ad7f5fd0b2b603deb34b1d9db052e570f63
Trisno <trisno@bibit.id>
2023-01-22 14:48:07 +0700

    add modal new transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expanses/widgets/user_transaction.dart';
+import 'package:personal_expanses/widgets/new_transaction.dart';
+import 'models/transaction.dart';
+import 'widgets/transasction_list.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,41 +10,89 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter App',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  final List<Transaction> _transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'T-Shirt', amount: 25.99, date: DateTime.now()),
+    Transaction(id: 't3', title: 'Bag', amount: 87.99, date: DateTime.now()),
+    Transaction(id: 't4', title: 'Jacket', amount: 78.99, date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String trxTitle, double trxAmount) {
+    final newTrx = Transaction(
+        id: DateTime.now().toString(),
+        title: trxTitle,
+        amount: trxAmount,
+        date: DateTime.now());
+
+    setState(() {
+      _transactions.add(newTrx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            child: NewTransaction(_addNewTransaction),
+            onTap: () {},
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter App'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: const <Widget>[
-              Card(
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Text('Chart!'),
-                ),
-                elevation: 5,
-                color: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+        actions: [
+          IconButton(
+              onPressed: () => _startAddNewTransaction(context),
+              icon: const Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            const Card(
+              child: SizedBox(
+                width: double.infinity,
+                child: Text('Chart!'),
               ),
-              UserTransaction()
-            ],
-          ),
-        ));
+              elevation: 5,
+              color: Colors.blue,
+            ),
+            TransactionList(_transactions)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
